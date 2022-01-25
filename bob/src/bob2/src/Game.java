96323cdc86371bc92cd78562b1e0c873d519b2c5
TheFarDoor <76043020+TheFarDoor@users.noreply.github.com>
2022-01-25 13:25:01 +0000

    Update Game.java

--- bob/src/bob2/src/Game.java
+++ bob/src/bob2/src/Game.java
@@ -2,11 +2,72 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.Properties;
 
-public class Game extends JFrame
+public class Game extends JPanel implements ActionListener, KeyListener, MouseListener
 {
 
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //756 pixels wide
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels tall
 
 
+    public Game() {
 
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+        setBackground(Color.black);
+        setDoubleBuffered(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
