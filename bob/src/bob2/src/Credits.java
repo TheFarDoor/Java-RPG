da0060f03bf809a817cbc68e2ce3f35979d4fcd3
TheFarDoor <76043020+TheFarDoor@users.noreply.github.com>
2022-01-27 09:27:41 +0000

    Update Credits.java

--- bob/src/bob2/src/Credits.java
+++ bob/src/bob2/src/Credits.java
@@ -4,6 +4,13 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.Properties;
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Class: Credits
+ * Description: This class can be accessed from the Menu class and shows who has contributed in the game.
+ * Input: There will be a back button that the user can press.
+ * Output: Shows the credits of the game.
+ */
+
 public class Credits extends JFrame
 {
 
